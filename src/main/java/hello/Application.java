16b1e41b14b3875443254cef841c8ca07ea7482c
Eric <eric@nimblerx.com>
2019-06-20 23:36:19 -0700

    did some things

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,38 @@
 package hello;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.Jedis;
+import redis.embedded.RedisServer;
 
 @SpringBootApplication
 public class Application {
 
+    private static RedisServer redisServer;
+    public static int port;
+
     public static void main(String[] args) {
+        try {
+            setUp();
+        } catch (Exception e) {
+            System.out.println("setup didn't work");
+            System.out.println(e.getLocalizedMessage());
+        }
         SpringApplication.run(Application.class, args);
+    }
+
+    public static void setUp() throws IOException {
+
+        // Take an available port
+//        ServerSocket s = new ServerSocket(0);
+//        port = s.getLocalPort();
+        port = 6379;
+        System.out.println("PORT IS: " + port);
+//        s.close();
+
+        redisServer = new RedisServer(port);
+        redisServer.start();
     }
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 @RestController
 public class GreetingController {
@@ -12,10 +13,14 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    // Configure JEDIS
+    Jedis jedis = new Jedis("localhost", Application.port);
+
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
         System.out.println("==== in greeting ====");
+        jedis.set("name-" + name, name);
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
